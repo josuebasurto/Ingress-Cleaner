0b1cd67197c42c293717d9c53a14f41d0bfcc14b
Flamur Kasa <flaflamuri@gmail.com>
2014-09-30 03:38:01 +0200

    Switch to Page Action
    The extension only made sense in one specific webpage so having it there
    all the time is useless
    This may require to update screenshots and recordings

--- IngressCleaner/processintel.js
+++ IngressCleaner/processintel.js
@@ -1,4 +1,24 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'ingress.com/intel' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
   console.log('Ingress Cleaner icon was clicked! (' + new Date + ")");
   chrome.tabs.executeScript(null, {file: "Ingresscleaner.js"});
 });
