32f14cf3e81ef1d885652a3a670f6340c678d464
Flamur Kasa <flaflamuri@gmail.com>
2015-03-11 00:04:58 +0100

    Cleaned old comments
    Please leave other comments as they may be useful later

--- IngressCleaner/IngressCleaner.js
+++ IngressCleaner/IngressCleaner.js
@@ -11,28 +11,3 @@ chrome.storage.sync.get({
 	else
 		mc.className = "";
 });
-/*
-var HUDelements = [
-	"player_stats",
-	"header",
-	"game_stats",
-	"geotools",
-	"comm",
-	"bottom_right_stack",
-	"portal_filter_header",
-	"footer"//,        .gmnoprint 
-	];
-
-if(window.getComputedStyle(document.getElementById("dashboard_container"), null)["left"] == "20px"){
-	HUDelements.forEach(function(e){if(e != null)document.getElementById(e).style.display = "none";});
-	var dash = document.getElementById("dashboard_container");
-	dash.style.top = dash.style.left = dash.style.right = dash.style.bottom = 0;
-}
-else{
-	HUDelements.forEach(function(e){if(e != null)document.getElementById(e).style.display = "";});
-	var dash = document.getElementById("dashboard_container");
-	dash.style.top = "100px"
-	dash.style.left = "20px"
-	dash.style.right = "20px"
-	dash.style.bottom = "65px";
-}*/
